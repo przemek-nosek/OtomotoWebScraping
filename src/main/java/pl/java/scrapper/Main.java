7cfa112e6810bec7f4917a05894c9a6f364d5f37
Przemek Nosek <przemek.nosek@o2.pl>
2021-06-29 22:24:09 +0200

    added functionality to save page content to a file

--- src/main/java/pl/java/scrapper/Main.java
+++ src/main/java/pl/java/scrapper/Main.java
@@ -6,6 +6,11 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -26,11 +31,34 @@ public class Main {
 
 
         Document doc = Jsoup.connect(url).get();
-
+        var setOfLinks = new HashSet<String>();
         for (Element element : doc.select("a.offer-title__link")) {
-//            Elements select = element.select("a[href]");
+            String href = element.attr("href");
+            if (!href.contains("otomoto.pl")) {
+                continue;
+            }
+            setOfLinks.add(element.attr("href"));
             System.out.println(element.attr("href"));
         }
+        setOfLinks.forEach(System.out::println);
+        System.out.println(setOfLinks.size());
 
+        var increment = new AtomicInteger(0);
+        setOfLinks.forEach(link -> {
+            try {
+                var document = Jsoup.connect(link).get();
+                Files.writeString(Path.of("abc\\test" + increment.incrementAndGet() + ".html"), document.outerHtml());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+
+
+        // https://jsoup.org/cookbook/extracting-data/attributes-text-html
+        // https://able.bio/DavidLandup/introduction-to-web-scraping-with-java-jsoup--641yfyl
+        // https://www.otomoto.pl/osobowe/?search%5Bfilter_float_price%3Afrom%5D=500000&search%5Bfilter_float_price%3Ato%5D=1000000
+        // https://jsoup.org/
+        // https://www.youtube.com/watch?v=tI1qGwhn_bs
+        // https://www.youtube.com/results?search_query=java+web+scraping
     }
 }
